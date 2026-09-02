a7de505646960bbda5147cea15b474b965a4def8
Tyrostir <karthikeyan.klu@gmail.com>
2026-09-02 19:09:09 +0000

    corrected the code to control gdshader from c#

--- projects/P00_HelloPhone/CubeGdShader.cs
+++ projects/P00_HelloPhone/CubeGdShader.cs
@@ -4,23 +4,33 @@ using System;
 public partial class CubeGdShader : MeshInstance3D
 {
 	// Called when the node enters the scene tree for the first time.
-	[Export] public Color ShaderTint { get; set; } = new Color(.5f, 1f, 2.5f);
+	[Export] public Color ShaderTint { get; set; } = new Color(.5f, 1f, 0.5f);
+	private Color prevShaderTint {get;set;} = new Color(); 
 	private MeshInstance3D mesh;
+	private ShaderMaterial shaderMat;
 	public override void _Ready()
 	{
-		var mesh = GetNode<MeshInstance3D>(".");
-		if (mesh.MaterialOverride is ShaderMaterial shaderMat)
-		{
-			shaderMat.SetShaderParameter("tint",
-				new Vector3(ShaderTint.R, ShaderTint.G, ShaderTint.B));
-			GD.Print($"Shader tint set to {ShaderTint}");
-		}else{
-			GD.Print($"mesh.MaterialOverride: {mesh.MaterialOverride}");
+		shaderMat = MaterialOverride as ShaderMaterial
+		?? GetSurfaceOverrideMaterial(0) as ShaderMaterial
+		?? GetActiveMaterial(0) as ShaderMaterial;
+		if(shaderMat is null){
+			GD.PushError($"{Name}: no shaderMaterial. " + 
+			$"MaterialOverride={MaterialOverride}. " + 
+			$"SurfaceOverride0={GetSurfaceOverrideMaterial(0)}.");
+			return;
 		}
+		
+		shaderMat.SetShaderParameter("tint", new Vector3(ShaderTint.R, ShaderTint.G, ShaderTint.B));
+		GD.Print($"Shader tint set to {ShaderTint}");
 	}
+	
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if(prevShaderTint != ShaderTint){
+			shaderMat.SetShaderParameter("tint", new Vector3(ShaderTint.R, ShaderTint.G, ShaderTint.B));
+			prevShaderTint = ShaderTint;
+		}
 	}
 }
