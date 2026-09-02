2a71ea1b40ccd955fdfb03e40bf15079703b8366
Tyrostir <karthikeyan.klu@gmail.com>
2026-09-02 17:45:14 +0000

    ch 0.10: gdscript first contact

---
+++ projects/P00_HelloPhone/cube_2.gd
@@ -0,0 +1,12 @@
+extends MeshInstance3D
+
+@export var degreesPerSecond: float = -90.0;
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	rotate_y(deg_to_rad(degreesPerSecond) * delta);
